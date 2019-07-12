bdc6e0806ce6b80a50a895327a00bc22442ea78d
tm-roamer <tm165822@163.com>
2019-07-12 13:07:32 +0800

    edit

--- src/router.js
+++ src/router.js
@@ -3,46 +3,51 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
+import main from '@/views/main.vue'
+
 const EMPTY_VIEW = { template: '<router-view />' };
 
 export default new Router({
-    routes: [{
-        path: '/',
-        component: EMPTY_VIEW,
-        children: [
-			{
-				path: '/main', meta: {name: 'main'},
-				component: () => import('./views/main'),
-				children: [
-					{
-						path: '/', redirect: 'basic'	// 默认配置成重定向第一个菜单
-					},
-					{
-						path: 'basic',
-						component: EMPTY_VIEW,
-						children: [
-							{
-								path: 'radio', meta: {name: '单选框'},
-								component: () => import('@/views/basic/radio'),
-							}
-						]
-					},
-					{
-						path: 'chart',
-						component: EMPTY_VIEW,
-						children: [
-							{
-								path: 'line', meta: {name: '线形图'},
-								component: () => import('@/views/charts/line'),
-							}
-						]
-					},
-					{
-						path: 'template',
-						component: EMPTY_VIEW,
-					}
-				]
-			},
-        ]
-    }]
+    routes: [
+    	{
+			path: '/', component: EMPTY_VIEW,
+			children: [
+				{
+					path: '/', redirect: '/main'	// 默认配置成重定向第一个菜单
+				},
+				{
+					path: '/main', meta: {name: 'main'}, component: main,
+					children: [
+						{
+							path: '/', redirect: 'basic'
+						},
+						{
+							path: 'basic',
+							component: EMPTY_VIEW,
+							children: [
+								{
+									path: 'radio', meta: {name: '单选框'},
+									component: () => import('@/views/basic/radio'),
+								}
+							]
+						},
+						{
+							path: 'chart',
+							component: EMPTY_VIEW,
+							children: [
+								{
+									path: 'line', meta: {name: '线形图'},
+									component: () => import('@/views/charts/line'),
+								}
+							]
+						},
+						{
+							path: 'template',
+							component: EMPTY_VIEW,
+						}
+					]
+				},
+			]
+		}
+    ]
 });
