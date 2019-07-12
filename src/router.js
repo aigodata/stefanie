f163885d7231752b52e233002114431dda9a50fc
tm-roamer <tm165822@163.com>
2019-07-12 14:26:08 +0800

    edit

--- src/router.js
+++ src/router.js
@@ -5,7 +5,42 @@ Vue.use(Router)
 
 import main from '@/views/main.vue'
 
+// 白垩纪
+const chalk = () => import('./views/theme/theme-chalk.vue')
+// 日食
+const mixiaoku = () => import('./views/theme/theme-mixiaoku.vue')
+
 const EMPTY_VIEW = { template: '<router-view />' };
+
+let children = [
+	{
+		path: '/', redirect: 'basic'
+	},
+	{
+		path: 'basic',
+		component: EMPTY_VIEW,
+		children: [
+			{
+				path: 'radio', meta: {name: '单选框'},
+				component: () => import('@/views/basic/radio'),
+			}
+		]
+	},
+	{
+		path: 'chart',
+		component: EMPTY_VIEW,
+		children: [
+			{
+				path: 'line', meta: {name: '线形图'},
+				component: () => import('@/views/charts/line'),
+			}
+		]
+	},
+	{
+		path: 'template',
+		component: EMPTY_VIEW,
+	}
+]
 
 export default new Router({
     routes: [
@@ -19,31 +54,15 @@ export default new Router({
 					path: '/main', meta: {name: 'main'}, component: main,
 					children: [
 						{
-							path: '/', redirect: 'basic'
+							path: '/', redirect: 'theme-chalk'
 						},
 						{
-							path: 'basic',
-							component: EMPTY_VIEW,
-							children: [
-								{
-									path: 'radio', meta: {name: '单选框'},
-									component: () => import('@/views/basic/radio'),
-								}
-							]
+							path: 'theme-chalk', name: 'theme-chalk', component: chalk, meta: {name: '主题: 白垩纪'},
+							children: children
 						},
 						{
-							path: 'chart',
-							component: EMPTY_VIEW,
-							children: [
-								{
-									path: 'line', meta: {name: '线形图'},
-									component: () => import('@/views/charts/line'),
-								}
-							]
-						},
-						{
-							path: 'template',
-							component: EMPTY_VIEW,
+							path: 'theme-mixiaoku', name: 'theme-mixiaoku', component: mixiaoku, meta: {name: '主题: 日食'},
+							children: children
 						}
 					]
 				},
---
+++ src/views/theme/theme-chalk.vue
@@ -0,0 +1,18 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script>
+  export default {
+    name: 'theme-chalk',
+    mounted() {
+      document.body.setAttribute('id', 'body-theme-chalk')
+      // setTimeout(_ => (this.$store.commit("loading", false)), 1000)
+    }
+  }
+</script>
+
+<style lang="scss">
+   // @import '../../lib/theme-chalk/index.scss';
+</style>
+
